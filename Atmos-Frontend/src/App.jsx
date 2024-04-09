38193b02ccddee9fd9d44371d933fdcfd5d7ab08
Le Tien Phat <91601109+Niko1444@users.noreply.github.com>
2024-04-09 13:39:28 +0700

    base plate for Atmos

--- Atmos-Frontend/src/App.jsx
+++ Atmos-Frontend/src/App.jsx
@@ -1,36 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+// Import Pages
+import Home from './routes/Home/Home'
+import Setting from './routes/Setting/Setting'
+
+// Import Not Found Page
+import NotFound from './routes/NotFound'
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0)
 
-  return (
-    // A template for fetching data from thingsboard server
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+	return (
+		<Routes>
+			{/* All main pages */}
+			<Route path="/" element={<Home />} />
+
+			{/* About */}
+			<Route path="/setting" element={<Setting />} />
+
+			{/* Error pages */}
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	)
 }
 
-export default App;
+export default App
--- api.md
+++ api.md
@@ -1,10 +1,10 @@
-// Get the channel feeds
+# Get the channel feeds
 GET https://api.thingspeak.com/channels/2465663/feeds.json?api_key=MP0MEWPWMADVCPMG&results=2
 
 
-// Read channel fields
+# Read channel fields
 GET https://api.thingspeak.com/channels/2465663/fields/1.json?api_key=MP0MEWPWMADVCPMG&results=2
 
 
-// Read channel realtime update
+# Read channel realtime update
 GET https://api.thingspeak.com/channels/2465663/status.json?api_key=MP0MEWPWMADVCPMG
